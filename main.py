afccd325bb4559f7bbfd07497d603da0a1a856bf
istiak <=>
2020-02-21 12:09:19 +0600

    news call added and implemented in main

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ config = {
 }
 
 db = MyDB(config)
-news = MyNews()
+base_url  ="http://www.prothomalo.com"
+hash_list = db.get_hash_list() 
+
+
+news = MyNews(base_url      =base_url,
+              hash_list     =hash_list,
+              must_contain  ="www.prothomalo.com",
+              target_class  ="link_overlay")
+
+news.fetch_news(base_url) # hear base url in enry point 
+
+db.insert_news(news.get_news_dict())
 # db.insert_news({'hash1':'news_link1','hash2':'news_link2'})
 # print(db.get_hash_list())
-print(db.get_all())
+# print(db.get_all())
+
--- newsManager.py
+++ newsManager.py
@@ -1,57 +1,79 @@
 # provide a dict of news_hashes and news_links
-# if gives a hash_list will avoid links with with those hashes 
-import requests 
+# if gives a hash_list will avoid links with with those hashes
+import requests
 import validators
 from bs4 import BeautifulSoup
 
-class MyNews :
-    _main_dict = {} #dict
-    _main_hash = []
-    _dir_list   =  [ ] 
-    _URL = "http://www.prothomalo.com"
-    def __init__(self,url):
-        pass 
 
-    def get_news(url,depth):
-    print("recursion depth",depth)
-    # check if url is valid and contains  prthom alos url if not return 
-    if validators.url(url) == True and "www.prothomalo.com" in url :
-        pass 
-    elif  validators.url(URL+url) == True:
-        url = URL+url
-        pass  
-    else :
-        print("abort dir url...")
-        return 
+class MyNews:
+    _main_dict = {}  # dict
+    _main_hash = []
+    _target_class = '' #'link_overlay'
+    # dir_list = []
+    _URL = '' #"http://www.prothomalo.com"
+    _must_contain = '' #"www.prothomalo.com"
+
+    def __init__(self ,base_url,hash_list,must_contain,target_class):
+        self._URL = base_url
+        self._main_hash = hash_list
+        self._must_contain = must_contain 
+        self._target_class = target_class
+
+        # pass
+
+
+    def get_news_dict(self):
+        return self._main_dict 
     
-    # cheack url alreay exista in hash_list 
-    if url in main_hash :
-        return 
-    else :
-        h = str(hash(url))
-        main_hash.append(h)
-    try :
-        response = requests.get(url = url) 
-    except :
-        return 
-    data = BeautifulSoup(response.text)
-    anchors = data.findAll('a',href=True)
-    for anchor in anchors :
-        if "class" in anchor.attrs:
-            if "link_overlay" in anchor["class"]:
-                t_url = URL + anchor.attrs["href"] 
-                h = str(hash(t_url))
-                main_dict[h] = t_url
-                if h not in main_hash:
-                    main_hash.append(h) 
-        else :
-            # if dir url recursive call 
-            h = str(hash(anchor["href"]))
-            if h in main_hash:
-                pass
-            else :
-                main_hash.append(h)
-                print(anchor["href"])
-                get_news(anchor["href"],depth+1)
-            
-    return "" 
+    def get_news_hash_list(self):
+        return self._main_hash 
+
+    # def set_news_hash_list(self,hash_list=[]):
+    #     return self._main_hash = hash_list        
+
+    def fetch_news(self, url, depth=0):
+        print("recursion depth", depth)
+        # check if url is valid and contains  prthom alos url if not return
+        if validators.url(url) == True and self._must_contain in url:
+            pass
+        elif validators.url(self._URL+url) == True:
+            url = self._URL+url
+            pass
+        else:
+            print("abort dir url...")
+            return
+
+        # cheack url alreay exista in hash_list
+        if url in self._main_hash:
+            return
+        else:
+            h = str(hash(url))
+            self._main_hash.append(h)
+        
+        try:
+            response = requests.get(url=url)
+        except:
+            return
+        
+        data = BeautifulSoup(response.text)
+        anchors = data.findAll('a', href=True)
+
+        for anchor in anchors:
+            if "class" in anchor.attrs:
+                if  self._target_class in anchor["class"]:
+                    t_url = self._URL + anchor.attrs["href"]
+                    h = str(hash(t_url))
+                    self._main_dict[h] = t_url
+                    if h not in self._main_hash:
+                        self._main_hash.append(h)
+            else:
+                # if dir url recursive call
+                h = str(hash(anchor["href"]))
+                if h in self._main_hash:
+                    pass
+                else:
+                    self._main_hash.append(h)
+                    print(anchor["href"])
+                    self.fetch_news(anchor["href"], depth+1)
+
+        return ""
